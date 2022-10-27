e49be6081643f4b9a7e13e3d09483d6bd7e774c4
Sangyups <feels4637@gmail.com>
2022-10-27 16:20:37 +0900

    feat: add default template

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,17 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+// 추후 router로 분리
+app.get("/", (req, res) => {
+  res.send({ status: "Hello, World!" });
+});
+
+app.post("/", (req, res) => {
+  res.send(req.body);
+});
+
 app.listen(port, () => {
   console.log(`listening to port ${port}!`);
 });
-app.get('/', (req, res) => res.json({status: "Hello World"}));
