d9fc02bf390af8d02890caf8790f9d1141c4defb
jungin0507 <78080841+jungin0507@users.noreply.github.com>
2022-10-30 20:54:49 +0900

    feat: setup github action workflow for deployment (#2)
    * chore: configure cd workflow
    
    * refactor: move config.js into src
    
    * chore: remove no-require-rule
    
    * chore: add dotenv
    
    * chore: add .env.example
    
    * refactor: change name SERVER_PORT to PORT
    
    * fix: update github action version
    
    Co-authored-by: Sangyups <feels4637@gmail.com>

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+const config = require("./src/config");
+
+module.exports = {
+  apps: [
+    {
+      name: "icebreaQ-api",
+      script: "dist/src/index.js",
+      env: {
+        ...config,
+        NODE_ENV: "production",
+      },
+    },
+  ],
+  deploy: {
+    production: {
+      user: process.env.SERVER_USER,
+      host: process.env.SERVER_HOST,
+      key: "deploy.key",
+      ref: "main",
+      repo: "git@github.com:Team-ILA/icebreaQ-server.git",
+      path: process.env.SERVER_PATH,
+      "post-deploy":
+        "npm install && \
+         npm run build && \
+         pm2 reload ecosystem.config.js",
+    },
+  },
+};
