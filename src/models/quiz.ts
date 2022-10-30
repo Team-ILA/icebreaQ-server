b382713b9b94d2fcd6a3784a840620f22e6a5739
RetepMil <lkslyj2@naver.com>
2022-10-30 21:20:56 +0900

    fix: quiz model explanation

--- src/models/quiz.ts
+++ src/models/quiz.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const quizSchema = new mongoose.Schema({
   id: String, // 추후 socket.io의 room id로 사용
   creator: String,
-  questions: Array, // [{q: "질문1", a: [{"답변1"}, {"답변2"}, ...]}, {q: "질문2", a: [{"답변1"}, {"답변2"}, ...]}, ...]
+  questions: Array, // [{q: "질문1", a: ["답변1", "답변2", ...]}, {q: "질문2", a: ["답변1", "답변2", ...]}, ...]
   published_date: { type: Date, default: Date.now },
 });
 
