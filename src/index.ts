70e46132f6d6055624c297118104813954373640
RetepMil <lkslyj2@naver.com>
2022-10-30 20:59:39 +0900

    chore: initialized mongoose setting

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import Dotenv from "dotenv";
+// import config from "../config";
+
+Dotenv.config();
+const { DB_USER, DB_PASSWORD, MONGO_URI } = process.env;
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// 추후 router로 분리
-app.get("/", (req, res) => {
-  res.send({ status: "Hello, World!" });
-});
+mongoose
+  .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${MONGO_URI}`)
+  .then(() => console.log("connection successful"))
+  .catch((e) => console.error(e));
 
-app.post("/", (req, res) => {
-  res.send(req.body);
-});
+// app.use('/users', usersRouter)
+// app.use('/quizzes', quizRouter)
 
 app.listen(port, () => {
   console.log(`listening to port ${port}!`);
