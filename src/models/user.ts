cbd9fed6f6ad8c957ea60f64e107d006aa7ad8f1
chany63 <chany63@korea.ac.kr>
2022-11-05 02:52:27 +0900

    fix: add username to schema

--- src/models/user.ts
+++ src/models/user.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 // 임시
 const userSchema = new mongoose.Schema({
   email: String,
+  username: String, 
   password: String, // Authentication 취약점으로 활용될 hash 값 보관
 });
 
