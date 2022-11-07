27020995677a63f0d6c9e9e152587801e7637550
Sangyups <feels4637@gmail.com>
2022-11-07 22:06:28 +0900

    feat: quiz schema 수정 및 필드 추가

--- src/models/quiz.ts
+++ src/models/quiz.ts
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
-// 임시
 const quizSchema = new mongoose.Schema({
-  quizId: String, // 추후 socket.io의 room id로 사용
-  creator: String,
-  QA: Array, // [{q: "질문1", a: ["답변1", "답변2", ...]}, {q: "질문2", a: ["답변1", "답변2", ...]}, ...]
-  current_question: Number,
+  // 추후 socket.io의 room id로 사용
+  quizId: { type: String, required: true },
+  creator: { type: String, required: true },
+  // [{q: "질문1", a: ["답변1", "답변2", ...]}, {q: "질문2", a: ["답변1", "답변2", ...]}, ...]
+  QA: Array,
+  current_question: { type: Number, required: true },
   published_date: { type: Date, default: Date.now },
-  active_users: Array,
+  active_users: { type: Array, default: [] },
+  title: { type: String, required: true },
+  limit: { type: Number, required: true },
 });
 
 export default mongoose.model("quiz", quizSchema);
