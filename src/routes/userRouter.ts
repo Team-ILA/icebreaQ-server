946decfeba01322e4a42d383db595889f37ea779
Sangyups <feels4637@gmail.com>
2022-11-07 19:22:50 +0900

    feat: clear cookie when logout

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -11,10 +11,8 @@ declare module "express-session" {
 const userRouter = express.Router();
 
 userRouter.post("/register", async (req, res, next) => {
-  // email validation(optional)
   try {
-    // try: error를 내는 부분
-    const { email, username, password } = req.body; // request의 body에서 email, username, password를 꺼내 구조분해 할당
+    const { email, username, password } = req.body;
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -33,17 +31,13 @@ userRouter.post("/register", async (req, res, next) => {
 
     res.status(201).json({ message: "created", email, username });
   } catch (err) {
-    // try문에서 던진 error를 catch해서 next()로  내보냄
     next(err);
   }
 });
 
 userRouter.post("/login", async (req, res, next) => {
   try {
-    // try: error를 내는 부분
-    const { email, password } = req.body; // request의 body에서 email, username, password를 꺼내 구조분해 할당
-    // email, hash(password)로 몽고디비에서 find
-    // 없다면 throw new Error('400');
+    const { email, password } = req.body;
     const exUser = await User.findOne({ email: email });
     if (!exUser) {
       throw new Error("400");
@@ -56,25 +50,16 @@ userRouter.post("/login", async (req, res, next) => {
 
     res.status(201).send({ email, username: exUser.username });
   } catch (err) {
-    // try문에서 던진 error를 catch해서 next()로  내보냄
     next(err);
   }
-  // 있다면,
-  // 몽고디비에서 받아온 데이터를 이용,
-  // req.session.user = email
-  // 로 user 저장 (보통 email을 저장하진 않는데 편의를 위해 이렇게 진행해도 될 것 같습니다.)
-
-  // 추후 front가 완성되면 res.redirect() 등으로 메인페이지로 redirect
-  // res.status(200).send();
 });
 
-userRouter.get("/logout", (req, res) => {
+userRouter.post("/logout", (req, res) => {
   req.session.destroy((err) => {
-    console.log(err);
+    if (err) throw new Error("500");
   });
 
-  // 추후 front가 완성되면 res.redirect() 등으로 메인페이지로 redirect
-  // res.status(200).send();
+  res.clearCookie("connect.sid");
   res.status(200).send();
 });
 
