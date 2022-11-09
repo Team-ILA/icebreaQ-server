cb1064e90fbd00b89543bab48927c07fbb2c363e
Sangyups <feels4637@gmail.com>
2022-11-09 16:33:32 +0900

    fix configure bindings

--- configure_bindings.sh
+++ configure_bindings.sh
@@ -6,14 +6,12 @@ npm install
 npx node-gyp configure && npx node-gyp build
 cp ./build/Release/emailValidator.node ../../../build/Release/emailValidator.node
 
-cd ../password-validator
-npm install
-npx node-gyp configure && npx node-gyp build
-cp ./build/Release/passwordValidator.node ../../../build/Release/passwordValidator.node
-
-
 cd ../logger
 npm install
 npx node-gyp configure && npx node-gyp build
 cp ./build/Release/log.node ../../../build/Release/log.node
 
+cd ../password-validator
+npm install
+npx node-gyp configure && npx node-gyp build
+cp ./build/Release/passwordValidator.node ../../../build/Release/passwordValidator.node
