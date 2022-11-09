92d358a1f32c67bdce211f4821c6a39ae5243965
jungin0507 <a01033046758@gmail.com>
2022-11-09 10:32:59 +0900

    feat: video chat socket events

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,16 @@ import Quiz from "./models/quiz";
 import apiRouter from "./routes/apiRouter";
 import session from "express-session";
 
+type QuizInfo = {
+  current_question: {
+    questionNum: number;
+    content: string;
+  };
+  answers: string[];
+  title: string;
+  creator: string;
+};
+
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
@@ -58,7 +68,7 @@ interface ISocket extends Socket {
 
 io.on("connection", (socket: ISocket) => {
   socket.on("greeting", async (data: any) => {
-    const { quizId, nickname } = data;
+    const { userId, quizId, nickname } = data;
 
     const quiz = await Quiz.findOne({ quizId });
 
@@ -70,15 +80,24 @@ io.on("connection", (socket: ISocket) => {
     await socket.join(quizId);
     socket.nickname = nickname;
     socket.quizId = quizId;
+    console.log(socket.quizId);
 
     await Quiz.updateOne(
       { quizId },
       { $set: { active_users: [...quiz.active_users, nickname] } },
     );
 
-    io.to(quizId).emit("greeting_response", {
-      roomMembers: [...quiz.active_users, nickname],
-    });
+    const quizInfo: QuizInfo = {
+      current_question: {
+        questionNum: quiz.current_question + 1,
+        content: quiz.QA[quiz.current_question].question,
+      },
+      answers: quiz.QA[quiz.current_question].answer,
+      title: quiz.title,
+      creator: quiz.creator,
+    };
+
+    io.to(quizId).emit("greeting_response", quizInfo);
   });
 
   socket.on("disconnecting", async () => {
@@ -126,6 +145,29 @@ io.on("connection", (socket: ISocket) => {
 
     io.to(quizId).emit("answer_submitted", {
       updatedAnswer: [...quiz.QA[currentQuestion].answer, newAnswer],
+    });
+  });
+
+  socket.on("join-room", (userData) => {
+    const { roomID, userID } = userData;
+    socket.join(roomID);
+    socket.broadcast.to(roomID).emit("new-user-connect", userData);
+    socket.on("disconnect", () => {
+      socket.broadcast.to(roomID).emit("user-disconnected", userID);
+    });
+    socket.on("broadcast-message", (message) => {
+      socket.broadcast
+        .to(roomID)
+        .emit("new-broadcast-messsage", { ...message, userData });
+    });
+    // socket.on('reconnect-user', () => {
+    //     socket.broadcast.to(roomID).emit('new-user-connect', userData);
+    // });
+    socket.on("display-media", (value) => {
+      socket.broadcast.to(roomID).emit("display-media", { userID, value });
+    });
+    socket.on("user-video-off", (value) => {
+      socket.broadcast.to(roomID).emit("user-video-off", value);
     });
   });
 });
